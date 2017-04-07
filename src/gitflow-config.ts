591ef42e8b9896a12cfb8e0d8e4bbc015fbc4be6
Christopher Woolum <christopher.woolum@gmail.com>
2017-04-07 17:08:20 -0600

    get some functionality going

--- src/gitflow-config.ts
+++ src/gitflow-config.ts
@@ -1,28 +1,74 @@
 import Controls = require("VSS/Controls");
 import Grids = require("VSS/Controls/Grids");
+import Dialogs = require("VSS/Controls/Dialogs");
+import RestClient = require("TFS/VersionControl/GitRestClient");
 
 export class GitflowConfig {
 
   constructor() {
-    var container = $("§build-grid-container");
 
-    var gridOptions: Grids.IGridOptions = {
-      height: "100%",
-      width: "100%",
-      source: () => {
-        var result = [], i;
-        for (i = 0; i < 100; i++) {
-          result[result.length] = [i, "Column 2 text" + i, "Column 3 " + Math.random()];
+  }
+
+  setupDialog() {
+    $("#show").click(() => {
+      // Display the dialog
+      var dialog = Dialogs.show(Dialogs.ModalDialog, <Dialogs.IModalDialogOptions>{
+        width: 300,
+        title: "Register",
+        content: $(".dialog-content").clone(),
+        okCallback: (result: any) => {
+          $("<li />").text(result).appendTo(".person-list");
         }
-        return result;
-      },
-      columns: [
-        { text: "Column 1", index: 0, width: 50 },
-        { text: "Column 2", index: 1, width: 200, canSortBy: false }
-      ]
+      });
 
-    };
+      var dialogElement = dialog.getElement();
+      // Monitor input changes
+      dialogElement.on("input", "input", (e: JQueryEventObject) => {
+        // Set dialog result
+        dialog.setDialogResult(getValue(dialogElement));
+        // Update enabled status of ok button
+        dialog.updateOkButton(!isEmpty(dialogElement));
+      });
+    });
 
-    Controls.create(Grids.Grid, container, gridOptions);
+    function isEmpty(parent: JQuery): boolean {
+      return parent.find("input").filter((index: number, el: Element) => {
+        return !$(el).val();
+      }).length > 0;
+    }
+
+    function getValue(parent: JQuery): string {
+      return parent.find("input").map((index: number, el: Element) => {
+        return $(el).val();
+      }).get().join(" - ");
+    }
+  }
+
+  buildGrid() {
+    var client = RestClient.getClient();
+
+    client.getRepositories().then(response => {
+      var container = $(".build-grid-container");
+
+      var gridOptions: Grids.IGridOptions = {
+        height: "100%",
+        width: "100%",
+        source: response.map(repo => repo.name),
+        columns: [
+          { text: "Name", index: 0 }
+        ]
+      };
+
+      Controls.create(Grids.Grid, container, gridOptions);
+    });
+
+  }
+
+  private buildSource() {
+    var result = [], i;
+    for (i = 0; i < 100; i++) {
+      result[result.length] = [i, "Column 2 text" + i];
+    }
+    return result;
   }
 }
