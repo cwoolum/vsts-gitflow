3c3e024447865f6a41beb91c955b0deeae6c73cc
Christopher Woolum <christopher.woolum@gmail.com>
2017-04-07 15:05:34 -0600

    more stuff

---
+++ dist/app.js
@@ -0,0 +1,10 @@
+define(["require", "exports"], function (require, exports) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    var GitflowConfig = (function () {
+        function GitflowConfig() {
+        }
+        return GitflowConfig;
+    }());
+    exports.GitflowConfig = GitflowConfig;
+});
